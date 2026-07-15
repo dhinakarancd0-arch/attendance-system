32e88985923bee8a466866d6b845779937943a7e
Dinakaran <dhina5567c@gmail.com>
2026-07-15 10:16:59 +0530

    Fix MongoDB environment variable

--- server/server.js
+++ server/server.js
@@ -3,21 +3,22 @@ const dotenv = require("dotenv");
 const app = require("./app");
 
 dotenv.config();
-console.log("Mongo URI:", process.env.MONGODB_URI);
+
+// Read the correct environment variable
+console.log("Mongo URI:", process.env.MONGO_URI);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
-
     console.log("Database:", mongoose.connection.name);
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
-
   .catch((err) => {
     console.error("❌ MongoDB Connection Error:", err.message);
   });
